9f8f8c0a7d8c4c16826fb7a25e8100ef80aec7cf
Hendrik Muhs <hendrik.muhs@gmail.com>
2017-01-13 22:55:36 +0100

    some stubs for indexer

---
+++ keyvi/src/cpp/index/index_writer.h
@@ -0,0 +1,20 @@
+/*
+ * index_writer.h
+ *
+ *  Created on: Jan 11, 2017
+ *      Author: hendrik
+ */
+
+#ifndef SRC_CPP_INDEX_INDEX_WRITER_H_
+#define SRC_CPP_INDEX_INDEX_WRITER_H_
+
+
+namespace keyvi {
+namespace index {
+
+} /* namespace index */
+} /* namespace keyvi */
+
+
+
+#endif /* SRC_CPP_INDEX_INDEX_WRITER_H_ */
---
+++ keyvi/tests/cpp/index/index_reader_test.cpp
@@ -0,0 +1,40 @@
+//
+// keyvi - A key value store.
+//
+// Copyright 2015 Hendrik Muhs<hendrik.muhs@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+
+/*
+ * index_reader_test.cpp
+ *
+ *  Created on: Jan 13, 2017
+ *      Author: hendrik
+ */
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include "index/index_reader.h"
+
+namespace keyvi {
+namespace dictionary {
+BOOST_AUTO_TEST_SUITE( IndexTests )
+
+BOOST_AUTO_TEST_CASE( loadIndex ) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} /* namespace dictionary */
+} /* namespace keyvi */
